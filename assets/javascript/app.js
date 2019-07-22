809abbb1e74d0f2e3391c59625b39832d5445502
edelBCS <edelm8@gmail.com>
2019-07-22 13:25:51 -0700

    inital file creation and html/js object

---
+++ assets/javascript/app.js
@@ -0,0 +1,25 @@
+var questions = [
+    {
+        questionNo : 1,
+        questionText : "text",
+        answers : ["answer1", "answer2", "answer3", "answer4"],
+        correctAns : 1
+    },
+    {
+        questionNo : 2,
+        questionText : "text",
+        answers : ["answer1", "answer2", "answer3", "answer4"],
+        correctAns : 1
+    },
+    {
+        questionNo : 3,
+        questionText : "text",
+        answers : ["answer1", "answer2", "answer3", "answer4"],
+        correctAns : 1
+    }
+]
+
+//Load first question and start timer
+//player answers correct show correct gif
+//if timer runs out or wrong question is marked wrong, show wrong gif
+//then load next question
