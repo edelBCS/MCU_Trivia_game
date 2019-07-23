a2ea1d016ff06290aeac30237b4286b8f6728ed5
edelBCS <edelm8@gmail.com>
2019-07-22 20:42:19 -0700

    still having issue with background image

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,14 +10,28 @@ var questions = [
         ],
         correctAns : 0,
         correctGif : "https://media.giphy.com/media/DjclSiVtiB11C/giphy.gif",
-        wrongGif : "https://media.giphy.com/media/wNbMrlmfatKqA/giphy.gif"
+        wrongGif : "https://media.giphy.com/media/wNbMrlmfatKqA/giphy.gif",
+        backgroundImg : "https://images.vice.com/vice/images/articles/meta/2015/05/15/entendiendo-el-traje-de-ironman-1431728701.jpg"
+    },
+    {
+        questionNo : 2,
+        questionText : "In which MCU movie did the character of Peter Parker first appear?",
+        answers : [
+            "Iron Man 2", 
+            "Spiderman: Homecoming", 
+            "Captain America: Civil War", 
+            "Avengers: Infinity Way"
+        ],
+        correctAns : 1,
+        correctGif : "https://66.media.tumblr.com/938ec4e7eba4d66a6b643b8815a5b3a4/tumblr_inline_p8ud2cI25d1tkli1e_500.gif",
+        wrongGif : "https://media.giphy.com/media/xT9IgoB4yOspMqzufu/giphy.gif"
     }
 ]
 
 var intervalId;
 var clockRunning = false;
 var gameOver = false;
-var timeLeft = 10;
+var timeLeft = 100;
 var score = 0;
 var currentQuestion = 0;
 
@@ -59,6 +73,14 @@ function endGame(){
 }
 
 function loadQuestion(index){
+    $("#background")
+        .css("background-image", "url(" + questions[currentQuestion].backgroundImg + ")")
+        .css("height", "-webkit-fill-available")
+        .css("background-size", "100%")
+        .css("opacity", "0.5")
+        .css("z-index", "0")
+        .css("top", "0");
+
     $("#question").empty();
     $("#question")
         .append($("<h2>")
